3498c0d83db5c3a0ad4ccefd383c3f4f9f046129
alptekin-mert <mrtlptkn45@gmail.com>
2023-06-07 14:28:23 +0300

    Post Page

--- src/app/pages/post-page/post-page.component.ts
+++ src/app/pages/post-page/post-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PostsService } from '../services/posts.service';
-import { Subscription } from 'rxjs';
+import { Subscription, forkJoin, switchMap } from 'rxjs';
 import { Posts } from '../models/posts';
 
 @Component({
@@ -18,11 +18,13 @@ export class PostPageComponent implements OnInit, OnDestroy {
     });
   }
 
-  posts: Posts[] = [];
-  ngOnInit(): void {
-    const sb = this.postService.getPosts().subscribe({
+  sb1!: Subscription;
+
+  loadData() {
+    this.postService.getPosts().subscribe({
       next: (data: Posts[]) => {
         console.log('data', data);
+
         // try
         // işlem başarılı ise network hatası yoksa veriyi buradan yakalarız
         this.posts = [...data];
@@ -37,6 +39,52 @@ export class PostPageComponent implements OnInit, OnDestroy {
       },
     });
 
-    this.subs.push(sb);
+    // this.postService.getPostById(1).subscribe((data2) => {
+    //   console.log('data2', data2);
+    //   console.log('this.posts', this.posts);
+    // });
+
+    // promise chain servisin bitiminde diğer servisi bağlayıp servis çağırısını sıralı senkton hale getirme kodu
+    // this.postService
+    //   .getPostById(1)
+    //   .pipe(
+    //     switchMap((data) => {
+    //       console.log('d-before-switchMap', data);
+    //       return this.postService.getPosts();
+    //     })
+    //   )
+    //   .subscribe((data3) => {
+    //     console.log('data3', data3);
+    //   });
+
+    // request aggregation, promise all
+    // forkJoin(this.postService.getPostById(1), this.postService.getPosts())
+    //   .pipe()
+    //   .subscribe((data4) => {
+    //     console.log('data4', data4);
+    //   });
+  }
+
+  posts: Posts[] = [];
+  ngOnInit(): void {
+    this.loadData();
+    // const sb = this.postService.getPosts().subscribe({
+    //   next: (data: Posts[]) => {
+    //     console.log('data', data);
+    //     // try
+    //     // işlem başarılı ise network hatası yoksa veriyi buradan yakalarız
+    //     this.posts = [...data];
+    //   },
+    //   error: (err) => {
+    //     // catch
+    //     console.log('err', err);
+    //   },
+    //   complete() {
+    //     // finally
+    //     // hata olsa da olmasada burası çalışır.
+    //   },
+    // });
+
+    // this.subs.push(this.sb1);
   }
 }
